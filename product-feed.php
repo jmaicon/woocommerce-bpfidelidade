37bbadc67ef0a62ad0f60c6f98fc3531597986f2
Jorge <eu@jorgemaicon.com.br>
2014-07-16 16:06:02 -0300

    list gallery images

--- product-feed.php
+++ product-feed.php
@@ -6,12 +6,14 @@
 header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
 $more = 1;
 
-echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
+echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
+
+$settings = get_option('wbpf_settings'); ?>
 
 <catalogo xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.bpfidelidade.com/schema/catalogo">
   <data><?php echo mysql2date('c', get_lastpostmodified('blog'), false); ?></data>
+  
   <parceiro>
-    <?php $settings = get_option('wbpf_settings'); ?>
     <codigo><?php echo $settings['wbpf_codigo_parceiro']; ?></codigo>
     <cnpj><?php echo $settings['wbpf_cnpj']; ?></cnpj>
     <nome><?php echo $settings['wbpf_nome_fornecedor']; ?></nome>
@@ -33,16 +35,35 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
       <promocional><?php echo $product->sale_price; ?></promocional>
       <custo></custo>
       <disponibilidade><?php echo $product->is_in_stock(); ?></disponibilidade>
-      <entrega>15</entrega>
+      <entrega><?php echo $settings['wbpf_entrega']; ?></entrega>
       <validade>2015-03-30T00:00:00</validade>
       <imagens>
-      	<!-- post thumbnail + gallery -->
         <imagem>
           <urlThumb><?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?></urlThumb>
           <urlDefault><?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?></urlDefault>
           <urlZoom><?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?></urlZoom>
           <principal>true</principal>
         </imagem>
+
+        <?php 
+
+          $attachment_ids = $product->get_gallery_attachment_ids();
+
+          if ( $attachment_ids ) {
+            
+            foreach ( $attachment_ids as $attachment_id ) {
+              
+              $image_link = wp_get_attachment_url( $attachment_id ); ?>
+              
+              <imagem>
+                <urlThumb><?php echo $image_link; ?></urlThumb>
+                <urlDefault><?php echo $image_link; ?></urlDefault>
+                <urlZoom><?php echo $image_link; ?></urlZoom>
+                <principal>false</principal>
+              </imagem> <?php
+            }
+          } 
+        ?>
       </imagens>
     </produto>
   <?php endwhile; ?>
